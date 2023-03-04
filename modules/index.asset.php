4650ab08e52dc4969350450fc4d6d4ed5e64a24f
golamrobbani <golamrobbani29@gmail.com>
2023-03-04 12:10:08 +0600

    add controls module

--- modules/index.asset.php
+++ modules/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7a51e8829b2ef4eb737d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '80d1fae525dc99359ef5');
