29039daf20e1a1dedbd6eb7c9299bcbecbbcfa73
asikurrahman75 <asikur@bdthemes.com>
2023-03-26 14:38:22 +0600

    typography control style

--- modules/index.asset.php
+++ modules/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '38e8faac2d38b80714d4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '54799b57e21b1a0240f2');
