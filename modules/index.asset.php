bafce1dbce582f9ad6099ffdad5735b789f27a02
Jamil Uddin <jamilbd07@gmail.com>
2023-04-05 01:06:01 +0600

    add fontawesome icon

--- modules/index.asset.php
+++ modules/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd8c877fbea2ec2c4ec5b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '1016c294aa3217f32d70');
--- src/controls/icon-picker/index.js
+++ src/controls/icon-picker/index.js
@@ -2,52 +2,101 @@ import { __ } from '@wordpress/i18n';
 import {
     PanelRow,
     Dashicon,
-    SearchControl
+    SearchControl,
+    TabPanel,
+    Popover
 } from '@wordpress/components';
-import { useEffect, useState } from '@wordpress/element';
+import { useEffect, useState, useRef } from '@wordpress/element';
 
 import "../scss/componets/_icon-picker.scss"
 
 
 //Import Dashicon list
 import { dashIcon } from "./icons/dashicon"
+import { fontAwesome } from "./icons/fontawesome"
+
+function useOutsideAlerter(ref, setVal) {
+    useEffect(() => {
+        /**
+         * Alert if clicked on outside of element
+         */
+        function handleClickOutside(event) {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setVal(false)
+            }
+        }
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [ref]);
+}
+
 
 const IconPicker = (props) => {
-
-    const [selectedIcon, setSelectedIcon] = useState("");
     const [searchInput, setSearchInput] = useState("");
+    const [iconType, setIconType] = useState("dashicon");
     const [icons, setIcons] = useState("");
+    const [showPopover, setShowPopover] = useState(false);
+    const [popoverAnchor, setPopoverAnchor] = useState("");
+
+    const popoverWrapperRef = useRef(null);
+    useOutsideAlerter(popoverWrapperRef, setShowPopover);
 
     useEffect(() => {
         if (typeof dashIcon === 'object' && Object.keys(dashIcon).length > 0) {
             setIcons(dashIcon);
         }
+
+        //set popover anchor
+        const selector = document.querySelector("#zoloIcon");
+        setPopoverAnchor(selector);
+
+
         // let icons = {}
-        // Object.keys(dashIcon).map((item, index) => {
+        // Object.keys(fontawesome).map((item, index) => {
+        //     const splitText = fontawesome[item].split(" ");
         //     icons = {
         //         ...icons,
-        //         [item]: {
-        //             name: item.replace(/-/g, " "),
-        //             source: 'dashicon',
-        //             type: ""
+        //         [splitText[1]]: {
+        //             name: splitText[1].replace(/-/g, " "),
+        //             source: 'fontawesome',
+        //             type: splitText[0]
         //         }
         //     }
         // })
         // console.log("Icons:", icons)
     }, [])
 
+    useEffect(() => {
+        switch (iconType) {
+            case "fontawesome":
+                if (typeof fontAwesome === 'object' && Object.keys(fontAwesome).length > 0) {
+                    setIcons(fontAwesome);
+                }
+                break;
+            default:
+                if (typeof dashIcon === 'object' && Object.keys(dashIcon).length > 0) {
+                    setIcons(dashIcon);
+                }
+        }
+    }, [iconType])
+
 
     const searchIcon = (text) => {
         setSearchInput(text)
 
         //Filter search result
-        const filteredIcons = Object.keys(dashIcon)
-        .filter((item) => item.includes(text))
-        .reduce((obj, key) => {
-            return Object.assign(obj, {
-                [key]: dashIcon[key]
-            })
-        }, {})
+        const iconList = iconType === 'fontawesome' ? fontAwesome : dashIcon
+        const filteredIcons = Object.keys(iconList)
+            .filter((item) => item.includes(text))
+            .reduce((obj, key) => {
+                return Object.assign(obj, {
+                    [key]: dashIcon[key]
+                })
+            }, {})
 
         //set Icons list from search result
         setIcons(filteredIcons)
@@ -56,20 +105,64 @@ const IconPicker = (props) => {
     return (
         <>
             <PanelRow>Select Icon</PanelRow>
-            <SearchControl
-                value={searchInput}
-                onChange={(text) => searchIcon(text)}
-            />
-            <div className='zolo-icon-area'>
-                {Object.keys(icons).map((item, index) => (
-                    <div className='zolo-icon-box'>
-                        <div className='zolo-icon-content'>
-                            <Dashicon icon={item} />
-                            <PanelRow>{dashIcon[item].name}</PanelRow>
-                        </div>
-                    </div>
-                ))}
+            <div id={"zoloIcon"}>
+                <Dashicon
+                    icon={'admin-media'}
+                    onClick={() => setShowPopover(true)}
+                />
             </div>
+            {showPopover && (
+                <Popover
+                    ref={popoverWrapperRef}
+                    anchor={popoverAnchor}
+                    className="zolo-iconpicker-popup"
+                >
+                    <SearchControl
+                        value={searchInput}
+                        onChange={(text) => searchIcon(text)}
+                    />
+                    <TabPanel
+                        className="zolo-parent-tab-panel"
+                        activeClass="active-tab"
+                        onSelect={(selected) => setIconType(selected)}
+                        tabs={[
+                            {
+                                name: 'dashicon',
+                                title: 'Dashicon',
+                                className: 'zolo-tab dashicon',
+                            },
+                            {
+                                name: 'fontawesome',
+                                title: 'FontAwesome',
+                                className: 'zolo-tab fontawesome',
+                            }
+                        ]}
+                    >
+                        {(tab) =>
+                            <div className='zolo-icon-area'>
+                                {
+                                    Object.keys(icons).map((item, index) => (
+                                        <div className='zolo-icon-box'>
+                                            <div className='zolo-icon-content'>
+                                                {iconType === 'dashicon' && (
+                                                    <Dashicon icon={item} />
+                                                )}
+                                                {iconType === 'fontawesome' && (
+                                                    <i class={`${icons[item].type} ${item}`}></i>
+                                                )}
+
+                                                <PanelRow>{icons[item].name}</PanelRow>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        }
+
+                    </TabPanel>
+
+                </Popover>
+            )}
 
         </>
     )
