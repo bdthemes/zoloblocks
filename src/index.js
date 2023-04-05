4ae166534108f2da897dcc6cdf2a4a46da9b7516
Zakaria PC <zakariapc@Zakarias-Mini.localdomain>
2023-04-05 10:59:30 +0600

    test changes

--- src/index.js
+++ src/index.js
@@ -6,3 +6,4 @@ import '../blocks/advanced-button/index.js';
 import '../blocks/advanced-heading/index.js';
 import '../blocks/advanced-image-gallery/index.js';
 import '../blocks/advanced-icon-box/index.js';
+import '../blocks/social-icon/index.js';
