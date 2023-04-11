835cc8cf1aa508cb9ea735d78b79f7fc66fac1ab
Muhibul Haque <muhibul@bdthemes.com>
2023-04-11 15:54:24 +0600

    Button Icon Added * Icon Position Change * Title Alignment

--- modules/index.asset.php
+++ modules/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f5b38f9a13049c75f7c4');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b6e2ae1402cdf437fbb3');
