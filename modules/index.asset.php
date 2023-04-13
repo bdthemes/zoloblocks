fc41db9cff7c8cc8a8420ccc9eb7aef9feb2f5ad
ZakariaBDT <zakaria@bdthemes.com>
2023-04-13 16:15:57 +0600

    modules files are added

--- modules/index.asset.php
+++ modules/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b7845ff057aaf20035b1');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b87d154695583a2a52e0');
