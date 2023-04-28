ad7d30d4bab0eff86e2927c657434e82c6650cd0
golamrobbani <golamrobbani29@gmail.com>
2023-04-28 08:12:31 +0600

    remove modules folder

--- modules/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c31c39387486485a2ded');
