ce161d6eaa7d7f2c1e388353a5329ec7800b16ae
ZakariaBDT <zakaria@bdthemes.com>
2023-05-28 10:23:28 +0600

    context is used

--- modules/index.asset.php
+++ modules/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f0daf96aede2bf23881d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '930ee055c3a556d2d8fd');
