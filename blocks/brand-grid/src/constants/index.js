f1a23c5a1874c1908bc7a9330dec3bc719343f3d
Muhibul Haque <muhibul@bdthemes.com>
2023-06-05 11:54:58 +0600

    Modules Folder Deleted

---
+++ blocks/brand-grid/src/constants/index.js
@@ -0,0 +1,187 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+// Block Prefix
+export const BLOCK_PREFIX = 'advanced-icon-box';
+// Presets
+export const PRESETS = [
+	{ label: __( 'Default', 'zolo-blocks' ), value: 'style-0' },
+	{ label: __( 'Preset 1', 'zolo-blocks' ), value: 'style-1' },
+	{ label: __( 'Preset 2', 'zolo-blocks' ), value: 'style-2' },
+	{ label: __( 'Preset 3', 'zolo-blocks' ), value: 'style-3' },
+];
+
+// title tag
+export const TITLE_TAG = [
+	{
+		label: 'H1',
+		value: 'h1',
+	},
+	{
+		label: 'H2',
+		value: 'h2',
+	},
+	{
+		label: 'H3',
+		value: 'h3',
+	},
+	{
+		label: 'H4',
+		value: 'h4',
+	},
+	{
+		label: 'H5',
+		value: 'h5',
+	},
+	{
+		label: 'H6',
+		value: 'h6',
+	},
+	{
+		label: 'Span',
+		value: 'span',
+	},
+	{
+		label: 'P',
+		value: 'p',
+	},
+];
+
+// icon positions
+export const ICON_BOX_POSITIONS = [
+	{
+		label: 'Left',
+		value: 'left',
+	},
+	{
+		label: 'Right',
+		value: 'right',
+	},
+	{
+		label: 'Top',
+		value: 'top',
+	},
+	{
+		label: 'Bottom',
+		value: 'bottom',
+	},
+];
+
+// icon positions
+export const ICON_POSITIONS = [
+	{
+		label: 'Left',
+		value: 'row-reverse',
+	},
+	{
+		label: 'Right',
+		value: 'row',
+	},
+	{
+		label: 'Top',
+		value: 'column-reverse',
+	},
+	{
+		label: 'Bottom',
+		value: 'column',
+	},
+];
+
+// button positions
+export const BUTTON_POSITIONS = [
+	{
+		label: 'Left',
+		value: 'left',
+	},
+	{
+		label: 'Center',
+		value: 'center',
+	},
+	{
+		label: 'Right',
+		value: 'right',
+	},
+];
+
+// side icon positions
+export const SIDE_ICON_POSITIONS = [
+	{
+		label: 'Top',
+		value: 'left',
+	},
+	{
+		label: 'Center',
+		value: 'center',
+	},
+	{
+		label: 'Bottom',
+		value: 'end',
+	},
+];
+
+// top icon positions
+export const TOP_ICON_POSITIONS = [
+	{
+		label: 'Left',
+		value: 'left',
+	},
+	{
+		label: 'Center',
+		value: 'center',
+	},
+	{
+		label: 'Right',
+		value: 'right',
+	},
+];
+// Icon BG
+export const ICON_BG_COLOR = 'iconNormal';
+// Icon Hover BG
+export const ICON_HOVER_BG_COLOR = 'iconHover';
+// Icon Typography
+export const ICON_TYPOGRAPHY = 'iconTypography';
+
+// Icon Padding
+export const ICON_PADDING = 'iconPadding';
+// Icon Margin
+export const ICON_MARGIN = 'iconMargin';
+// Icon Alignment
+export const ICON_BOX_ALIGNMENT = 'iconBoxAlignment';
+// Icon Border
+export const ICON_BORDER = 'iconBorder';
+// Icon Box Shadow
+export const ICON_BOX_SHADOW = 'iconBoxShadow';
+// Icon Hover Box Shadow
+export const ICON_HOVER_BOX_SHADOW = 'iconHoverBoxShadow';
+// Button Box Shadow
+export const BUTTON_BOX_SHADOW = 'buttonBoxShadow';
+// Button Hover Box Shadow
+export const BUTTON_HOVER_BOX_SHADOW = 'buttonHoverBoxShadow';
+// Icon Border Radius
+export const ICON_BORDER_RADIUS = 'iconBorderRadius';
+// Icon Size
+export const ICON_SIZE = 'iconSize';
+// Button Icon Size
+export const BUTTON_ICON_SIZE = 'buttonIconSize';
+// Button
+export const BUTTON_BORDER = 'buttonBorder';
+export const BUTTON_BORDER_RADIUS = 'buttonBorderRadius';
+export const BUTTON_MARGIN = 'buttonMargin';
+export const BUTTON_PADDING = 'buttonPadding';
+// icon and spacing
+export const ICON_SPACING = 'iconSpacing';
+// icon and text spacing
+export const ICON_TEXT_SPACING = 'iconTextSpacing';
+// title Alignment
+export const TITLE_ALIGNMENT = 'titleAlignment';
+// title margin
+export const TITLE_MARGIN = 'titleMargin';
+// description margin
+export const DESCRIPTION_MARGIN = 'descMargin';
+// description Alignment
+export const DESC_ALIGNMENT = 'descAlignment';
+// title controls
+export const TITLE_TEXT_SHADOW = 'titleTextShadow';
+export const TITLE_TEXT_STROKE = 'titleTextStroke';
