e3f8e2ebbc5db7ed3590cf101a3595b51e66a8ef
ZakariaBDT <zakaria@bdthemes.com>
2023-07-04 12:34:39 +0600

    merging with jamil branch

--- blocks/advanced-button/frontend/index.js
+++ blocks/advanced-button/frontend/index.js
@@ -1,1 +1,9 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************************!*\
+  !*** ./blocks/advanced-button/src/frontend.js ***!
+  \************************************************/
 console.log("Zolo Advanced Button");
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
--- zolo-blocks.php
+++ zolo-blocks.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Zolo Blocks
  * Plugin URI: https://bdthemes.com/
