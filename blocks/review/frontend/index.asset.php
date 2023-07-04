ca6a5658f759c4c0f88867373888a313b42596ee
ZakariaBDT <zakaria@bdthemes.com>
2023-07-04 11:15:38 +0600

    profile card issue is fixed

--- blocks/review/frontend/index.asset.php
+++ blocks/review/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '4000ecd628b32dc8e333');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'b2e4d548042f8c39b126');
