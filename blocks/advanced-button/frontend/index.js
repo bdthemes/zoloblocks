0127834b3c5e99a36f52ac9f6db7f580b3feae18
ZakariaBDT <zakaria@bdthemes.com>
2023-07-05 12:09:11 +0600

    header tab is added

--- blocks/advanced-button/frontend/index.js
+++ blocks/advanced-button/frontend/index.js
@@ -1,10 +1,9 @@
-/******/ (function () {
-	// webpackBootstrap
-	var __webpack_exports__ = {};
-	/*!************************************************!*\
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************************************!*\
   !*** ./blocks/advanced-button/src/frontend.js ***!
   \************************************************/
-	console.log('Zolo Advanced Button');
-	/******/
-})();
+console.log("Zolo Advanced Button");
+/******/ })()
+;
 //# sourceMappingURL=index.js.map
