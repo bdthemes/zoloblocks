48652b06676a7d93c3194263cc4e121f2431cdb7
ZakariaBDT <zakaria@bdthemes.com>
2023-07-27 10:32:49 +0600

    merging

--- blocks/review/frontend/index.asset.php
+++ blocks/review/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '8cedcfebfe6cf1206e09');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'a731337b1e1114557c6a');
