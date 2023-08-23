e761eab93ac3714d3ccca69ac45c76a5c0c7bd20
ZakariaBDT <zakaria@bdthemes.com>
2023-08-23 16:15:20 +0600

    js issues fixing

--- blocks/review/frontend/index.asset.php
+++ blocks/review/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => 'ed2d691b90c03326cfb8');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'a731337b1e1114557c6a');
