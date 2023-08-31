f2a1996c49476a53e9c9b13d4059b22d38184026
ZakariaBDT <zakaria@bdthemes.com>
2023-08-31 17:20:59 +0600

    adding blocks icons

--- blocks/social-icon/src/attributes.js
+++ blocks/social-icon/src/attributes.js
@@ -1,142 +1,142 @@
 const {
-	generateResRangeAttributies,
-	generateBorderAttributies,
-	generateResCounterAttributies,
-	generateDimensionAttributes,
-	generateBoxShadowAttributies,
-	generateTypographyAttributes,
+    generateResRangeAttributies,
+    generateBorderAttributies,
+    generateResCounterAttributies,
+    generateDimensionAttributes,
+    generateBoxShadowAttributies,
+    generateTypographyAttributes,
 } = window.zoloModule;
 
 import {
-	BUTTON_PADDING,
-	BUTTON_BORDER,
-	BTN_BORDER_RADIUS,
-	BTN_SHADOW,
-	BTN_HOVER_SHADOW,
-	ICON_TEXT_SPACING,
-	COLUMN_COUNT,
-	COLUMNS_GAP,
-	ROW_GAP,
-	BUTTON_SIZE,
-	BLOCK_MARGIN,
+    BUTTON_PADDING,
+    BUTTON_BORDER,
+    BTN_BORDER_RADIUS,
+    BTN_SHADOW,
+    BTN_HOVER_SHADOW,
+    ICON_TEXT_SPACING,
+    COLUMN_COUNT,
+    COLUMNS_GAP,
+    ROW_GAP,
+    BUTTON_SIZE,
+    BLOCK_MARGIN,
 } from './constants';
 
 import * as typographyObjs from './constants/typoPrefixConstant';
 
 const attributes = {
-	//Common Attributes
-	uniqueId: {
-		type: 'string',
-	},
-	resDevice: {
-		type: 'string',
-		default: 'Desktop',
-	},
-	blockStyle: {
-		type: 'object',
-	},
-	// border attributes
-	...generateBorderAttributies(BUTTON_BORDER),
-	...generateDimensionAttributes(BTN_BORDER_RADIUS),
-	...generateBoxShadowAttributies(BTN_SHADOW),
-	...generateBoxShadowAttributies(BTN_HOVER_SHADOW),
-	// column count
-	...generateResCounterAttributies(COLUMN_COUNT, {
-		defaultRange: 4,
-	}),
-	//columns gaps
-	...generateResRangeAttributies(COLUMNS_GAP),
-	//row gaps
-	...generateResRangeAttributies(ROW_GAP),
+    //Common Attributes
+    uniqueId: {
+        type: 'string',
+    },
+    resDevice: {
+        type: 'string',
+        default: 'Desktop',
+    },
+    blockStyle: {
+        type: 'object',
+    },
+    // border attributes
+    ...generateBorderAttributies(BUTTON_BORDER),
+    ...generateDimensionAttributes(BTN_BORDER_RADIUS),
+    ...generateBoxShadowAttributies(BTN_SHADOW),
+    ...generateBoxShadowAttributies(BTN_HOVER_SHADOW),
+    // column count
+    ...generateResCounterAttributies(COLUMN_COUNT, {
+        defaultRange: 4,
+    }),
+    //columns gaps
+    ...generateResRangeAttributies(COLUMNS_GAP),
+    //row gaps
+    ...generateResRangeAttributies(ROW_GAP),
 
-	//button
-	...generateDimensionAttributes(BUTTON_PADDING),
-	...generateResRangeAttributies(BUTTON_SIZE),
-	//icon spacing
-	...generateResRangeAttributies(ICON_TEXT_SPACING, {
-		default: 5,
-	}),
-	// block margin
-	...generateDimensionAttributes(BLOCK_MARGIN),
-	...generateTypographyAttributes(Object.values(typographyObjs)),
-	//Block specific Attributes
-	preset: {
-		type: 'string',
-		default: 'preset-1',
-	},
-	socialText: {
-		type: 'string',
-		default: 'iconText',
-	},
-	socialProfiles: {
-		type: 'array',
-		default: [
-			{
-				id: 1,
-				icon: {
-					facebook: {
-						name: 'facebook',
-						source: 'dashicon',
-						type: '',
-					},
-				},
-				link: {
-					url: '#',
-					openInNewTab: false,
-				},
-				text: 'Facebook',
-			},
-		],
-	},
-	socialProfilesLinkTarget: {
-		type: 'boolean',
-		default: true,
-	},
-	socialStyle: {
-		type: 'string',
-		default: 'default',
-	},
-	targetPage: {
-		type: 'string',
-	},
-	customLink: {
-		type: 'string',
-	},
-	socialColor: {
-		type: 'string',
-		default: 'original',
-	},
-	socialTextColor: {
-		type: 'string',
-	},
-	socialTextHoverColor: {
-		type: 'string',
-	},
-	socialBgColor: {
-		type: 'string',
-	},
-	socialBgHoverColor: {
-		type: 'string',
-	},
-	icon: {
-		type: 'string',
-	},
-	iconPosition: {
-		type: 'string',
-		default: 'right',
-	},
-	textColor: {
-		type: 'string',
-	},
-	textHoverColor: {
-		type: 'string',
-	},
-	borderHoverColor: {
-		type: 'string',
-	},
-	presetBgColor: {
-		type: 'string',
-	},
+    //button
+    ...generateDimensionAttributes(BUTTON_PADDING),
+    ...generateResRangeAttributies(BUTTON_SIZE),
+    //icon spacing
+    ...generateResRangeAttributies(ICON_TEXT_SPACING, {
+        default: 5,
+    }),
+    // block margin
+    ...generateDimensionAttributes(BLOCK_MARGIN),
+    ...generateTypographyAttributes(Object.values(typographyObjs)),
+    //Block specific Attributes
+    preset: {
+        type: 'string',
+        default: 'preset-1',
+    },
+    socialText: {
+        type: 'string',
+        default: 'iconText',
+    },
+    socialProfiles: {
+        type: 'array',
+        default: [
+            {
+                id: 1,
+                icon: {
+                    facebook: {
+                        name: 'facebook',
+                        source: 'dashicon',
+                        type: '',
+                    },
+                },
+                link: {
+                    url: '#',
+                    openInNewTab: false,
+                },
+                text: 'Facebook',
+            },
+        ],
+    },
+    socialProfilesLinkTarget: {
+        type: 'boolean',
+        default: true,
+    },
+    socialStyle: {
+        type: 'string',
+        default: 'default',
+    },
+    targetPage: {
+        type: 'string',
+    },
+    customLink: {
+        type: 'string',
+    },
+    socialColor: {
+        type: 'string',
+        default: 'original',
+    },
+    socialTextColor: {
+        type: 'string',
+    },
+    socialTextHoverColor: {
+        type: 'string',
+    },
+    socialBgColor: {
+        type: 'string',
+    },
+    socialBgHoverColor: {
+        type: 'string',
+    },
+    icon: {
+        type: 'string',
+    },
+    iconPosition: {
+        type: 'string',
+        default: 'right',
+    },
+    textColor: {
+        type: 'string',
+    },
+    textHoverColor: {
+        type: 'string',
+    },
+    borderHoverColor: {
+        type: 'string',
+    },
+    presetBgColor: {
+        type: 'string',
+    },
 };
 
 export default attributes;
--- blocks/social-icon/src/index.js
+++ blocks/social-icon/src/index.js
@@ -7,7 +7,18 @@ import Save from './save';
 import './style.scss';
 
 registerBlockType(metadata, {
-	attributes,
-	edit: Edit,
-	save: Save,
+    icon: {
+        src: (
+            <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx={7} cy={12} r="4.25" stroke="#4D4D4D" strokeWidth="1.5" />
+                <circle cx="18.5" cy="5.5" r="2.75" stroke="#4D4D4D" strokeWidth="1.5" />
+                <circle cx="18.5" cy="18.5" r="2.75" stroke="#4D4D4D" strokeWidth="1.5" />
+                <path d="M16 17L11 14" stroke="#4D4D4D" strokeWidth="1.5" />
+                <path d="M16 7L11 10" stroke="#4D4D4D" strokeWidth="1.5" />
+            </svg>
+        ),
+    },
+    attributes,
+    edit: Edit,
+    save: Save,
 });
