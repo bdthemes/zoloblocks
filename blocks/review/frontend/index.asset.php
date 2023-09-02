addcac232c59fc9cd106474e1d34d4564702fa2d
ZakariaBDT <zakaria@bdthemes.com>
2023-09-02 17:48:17 +0600

    blocks icons are added

--- blocks/review/frontend/index.asset.php
+++ blocks/review/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-element'), 'version' => '3b964cf04d37d6814d96');
+<?php return array('dependencies' => array('wp-element'), 'version' => 'a731337b1e1114557c6a');
