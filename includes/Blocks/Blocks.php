ff54b77a015d2fd3d910621fe16b9aa2ccfa1820
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-10-04 12:04:06 +0600

    Fontawesome frontend Fixed

--- includes/Blocks/Blocks.php
+++ includes/Blocks/Blocks.php
@@ -103,7 +103,7 @@ return apply_filters( 'zolo_blocks_list_data', [
     'counter' => [
         'name'             => 'counter',
         'metadata'         => trailingslashit( ZOLO_DIR_PATH ) . 'blocks/counter/block.json',
-        'frontend-styles'  => ['zolo-block-common-style'],
+        'frontend-styles'  => ['zolo-fontawesome','zolo-block-common-style'],
         'frontend-scripts' => ['zolo-counter-frontend', 'zolo-block-editor-dependency']
     ],
 
