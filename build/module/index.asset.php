484ca7e5c31a959d74285217e6759641de0b627e
Jamil Uddin <jamilbd07@gmail.com>
2023-10-13 18:21:41 +0600

    updated build

--- build/module/index.asset.php
+++ build/module/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3fdf172d7f9a04332970');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '69d58d22338207ebbabe');
