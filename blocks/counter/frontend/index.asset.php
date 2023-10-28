1afc4ce0bafaaa72e1f8a66303f14bf4d57e6949
Asikur Rahman <asikur@bdthemes.com>
2023-10-28 16:43:48 +0600

    typography & gradient issue fix

--- blocks/counter/frontend/index.asset.php
+++ blocks/counter/frontend/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'f7cdb6b6fa3c981b385d');
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => 'c27a9e0afe1cff1e13dc');
