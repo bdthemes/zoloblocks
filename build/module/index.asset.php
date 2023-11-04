eefb92170cb9dd170562e7d6542b7b2dc3ca6f72
ZakariaBDT <zakaria@bdthemes.com>
2023-11-04 15:04:49 +0600

    all minor issues are fixed

--- build/module/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9a2a96581028098ad791');
