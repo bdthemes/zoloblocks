b77c3a7f608b353800cf6cab8f520bbe2b816a8c
ZakariaBDT <zakaria@bdthemes.com>
2024-02-21 14:46:53 +0600

    cta block background issue fixed, thumbnail placeholder url fixed, flipbox block issue fixed

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ import '../blocks/team-child/src';
 import '../blocks/team-child/src';
 import '../blocks/team-grid/src';
 import '../blocks/team-grid/src';
-import '../blocks/list/src';
+// import '../blocks/list/src';
 
 // inner blocks
 // import '../blocks/text-field/src';
