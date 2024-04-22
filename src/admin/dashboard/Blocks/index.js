3044b876f55f0bc813b86bb5a2602edb1e83f62b
ABU TALIB <abutalib4590@gmail.com>
2024-04-22 11:50:01 +0600

    update ready

--- src/admin/dashboard/Blocks/index.js
+++ src/admin/dashboard/Blocks/index.js
@@ -38,6 +38,7 @@ const Blocks = () => {
     // set blocks list
     useEffect(() => {
         if (blocks.length > 0) {
+            blocks.sort((a, b) => a.title.localeCompare(b.title));
             setBlockStates(blocks);
         }
     }, [blocks]);
