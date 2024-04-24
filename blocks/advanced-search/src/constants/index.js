eb3954c1fd401a537791961b3ec8c988c180257e
Asikur Rahman <asikur@bdthemes.com>
2024-04-24 12:18:46 +0600

    search block preset add

--- blocks/advanced-search/src/constants/index.js
+++ blocks/advanced-search/src/constants/index.js
@@ -9,6 +9,7 @@ export const BLOCK_PREFIX = "advanced-search";
 export const PRESETS = [
   { label: __("Preset 1", "zoloblocks"), value: "zolo-search-1" },
   { label: __("Preset 2", "zoloblocks"), value: "zolo-search-2" },
+  { label: __("Preset 3", "zoloblocks"), value: "zolo-search-3" },
 ];
 export const BUTTON_TYPES = [
   { label: __("Text", "zoloblocks"), value: "text" },
