985ff955dfdac5b53d7771f4623d665c7bc85d43
ABU TALIB <abutalib4590@gmail.com>
2024-05-16 18:45:58 +0600

    charts issue fixed

--- includes/Classes/ZoloEnqueues.php
+++ includes/Classes/ZoloEnqueues.php
@@ -222,9 +222,9 @@ if (!class_exists('Zolo_Block_Enqueue')) {
                 wp_enqueue_script('zolo-tabs-frontend', trailingslashit(ZOLO_ADMIN_URL) . 'assets/js/tabs/tabify.js', [], ZOLO_VERSION, true);
             }
 
-            if (has_block('zolo/charts')) {
-                wp_enqueue_script('zolo-charts-lib-frontend', trailingslashit(ZOLO_ADMIN_URL) . 'assets/js/apexcharts/apexcharts.min.js', [], ZOLO_VERSION, true);
-            }
+            // if (has_block('zolo/charts')) {
+            //     wp_enqueue_script('zolo-charts-lib-frontend', trailingslashit(ZOLO_ADMIN_URL) . 'assets/js/apexcharts/apexcharts.min.js', [], ZOLO_VERSION, true);
+            // }
 
             // load gsap from cdn
             wp_enqueue_script('zolo-transform-effects', trailingslashit(ZOLO_ADMIN_URL) . '/build/animation/index.js', [], ZOLO_VERSION, true);
