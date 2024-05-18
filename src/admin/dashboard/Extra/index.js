2b8879d401edd5b2b0eedfec6fcb0ba85e94731a
ABU TALIB <abutalib4590@gmail.com>
2024-05-18 15:03:20 +0600

    integrate frill changelog

--- src/admin/dashboard/Extra/index.js
+++ src/admin/dashboard/Extra/index.js
@@ -1,11 +1,13 @@
 import CurrentVersion from './current-verison';
 import ChangeLogs from './change-logs';
+import FrillWidget from './components/FrillWidget';
 
 const ExtraInfo = () => {
     return (
         <div className="zolo-extra-info">
             <CurrentVersion version={zoloBlocks.plugin_version} />
-            <ChangeLogs />
+            {/* <ChangeLogs /> */}
+            <FrillWidget />
         </div>
     );
 };
