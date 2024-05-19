c5148245d2102ecf5c5a6fd33a3670ef199c4015
ABU TALIB <abutalib4590@gmail.com>
2024-05-19 10:54:46 +0600

    removed old log

--- src/admin/dashboard/Extra/index.js
+++ src/admin/dashboard/Extra/index.js
@@ -1,12 +1,10 @@
 import CurrentVersion from './current-verison';
-import ChangeLogs from './change-logs';
 import FrillWidget from './components/FrillWidget';
 
 const ExtraInfo = () => {
     return (
         <div className="zolo-extra-info">
             <CurrentVersion version={zoloBlocks.plugin_version} />
-            {/* <ChangeLogs /> */}
             <FrillWidget />
         </div>
     );
