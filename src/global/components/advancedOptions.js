628fdde7993587775d623f905f035d9d71a11dd4
ABU TALIB <abutalib4590@gmail.com>
2024-06-05 16:26:48 +0600

    counter issue fixed

--- src/global/components/advancedOptions.js
+++ src/global/components/advancedOptions.js
@@ -20,7 +20,6 @@ import TabPanelControl from '../../controls/tabpanel-control';
 import ResRangeControl from '../../controls/res-range-control';
 import ResAlignmentControl from '../../controls/res-alignment-control';
 import IconicBtnGroup from '../../controls/iconic-btn-group';
-import SimpleRangeControl from '../../controls/simple-range-control';
 import { applyFilters } from '@wordpress/hooks';
 import {
     DEFAULT_ALIGNS,
@@ -34,7 +33,6 @@ import {
     VPOSITIONS,
     CONTENT_POSITIONS,
 } from '../constants';
-import { zoloArraysMergeIfUniqueValue } from '../../helpers/helper';
 // value check
 const hasValCheck = (att, attributes) => {
     const { [`zolo_${att}Range`]: deskAtt, [`zolo_TAB${att}Range`]: tabAtt, [`zolo_MOB${att}Range`]: mobAtt } = attributes;
@@ -78,15 +76,8 @@ export const AdvancedOptions = (props) => {
         zoloId,
         overflow,
         position,
-        horizontalOrientation,
-        verticalOrientation,
-        positionLeft,
-        positionRight,
-        positionTop,
-        positionBottom,
     } = attributes;
 
-    console.log('attributes', position);
 
     const handleResponsiveness = (key, value, classname) => {
         let updatedClasses = [...parentClasses, classname];
