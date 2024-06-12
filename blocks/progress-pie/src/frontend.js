1c50097d9f1bd0d7f71c24efd83135902f696da2
bdtu027 <bdtu027@bdtu027s-Mini.localdomain>
2024-06-12 18:05:47 +0600

    again frontend class added

--- blocks/progress-pie/src/frontend.js
+++ blocks/progress-pie/src/frontend.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', () => {
               <>
                   <svg className="progress-pie" width="100%" height="100%" viewBox="0 0 42 42">
                   {/*  optional background if need  */}
-                  <circle className="donut-hole" cx="21" cy="21" r="15.91549430918954" ></circle> 
+                  <circle className="donut-hole progress-donut-hole" cx="21" cy="21" r="15.91549430918954" ></circle> 
                   <circle className="progress-pie-fill" cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke={progressFillColor ? progressFillColor :'#e5e5e5'}  stroke-dasharray="100 0" stroke-dashoffset="25"></circle>
                   <circle id="progress1" className="progress-pie-progress" ref={progress} cx="21" cy="21" r="15.91549430918954" fill="transparent" stroke="url(#gradient)"  stroke-dasharray="0 100" stroke-dashoffset="25"></circle>
                   
