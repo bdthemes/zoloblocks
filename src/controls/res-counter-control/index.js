f4cfb6d79d77d466fde5568110ac33aa29ddd0ed
ABU TALIB <abutalib4590@gmail.com>
2024-06-13 12:41:37 +0600

    make note changeable for res counter control

--- src/controls/res-counter-control/index.js
+++ src/controls/res-counter-control/index.js
@@ -4,7 +4,7 @@ import ResetBtn from '../reset-btn';
 import { prefix } from '../../global/constants';
 import { __ } from '@wordpress/i18n';
 
-const ResCounterControl = ({ label, controlName, requiredProps, min, max, step, defaults = {} }) => {
+const ResCounterControl = ({ label, controlName, requiredProps, min, max, step, defaults = {}, noteText="" }) => {
     const { attributes, setAttributes, resMode } = requiredProps;
     const {
         [`${prefix}${controlName}Range`]: desktopRange = defaults.deskRange || 3,
@@ -73,7 +73,7 @@ const ResCounterControl = ({ label, controlName, requiredProps, min, max, step, 
                             </div>
                             <p className="zb-counter-note">
                                 <strong>{__('Note: ', 'zita-blocks')}</strong>
-                                {__('maximum ', 'zita-blocks') + max + ' ' + __('minimum ') + min}
+                                {noteText || __('maximum ', 'zita-blocks') + max + ' ' + __('minimum ') + min}
                             </p>
                         </div>
                     </WithResDeviceBtn>
@@ -127,7 +127,7 @@ const ResCounterControl = ({ label, controlName, requiredProps, min, max, step, 
                         </div>
                         <p className="zb-counter-note">
                             <strong>{__('Note: ', 'zita-blocks')}</strong>
-                            {__('maximum ', 'zita-blocks') + max + ' ' + __('minimum ') + min}
+                            {noteText || __('maximum ', 'zita-blocks') + max + ' ' + __('minimum ') + min}
                         </p>
                     </div>
                 </WithResDeviceBtn>
@@ -180,7 +180,7 @@ const ResCounterControl = ({ label, controlName, requiredProps, min, max, step, 
                         </div>
                         <p className="zb-counter-note">
                             <strong>{__('Note: ', 'zita-blocks')}</strong>
-                            {__('maximum ', 'zita-blocks') + max + ' ' + __('minimum ') + min}
+                            {noteText || __('maximum ', 'zita-blocks') + max + ' ' + __('minimum ') + min}
                         </p>
                     </div>
                 </WithResDeviceBtn>
