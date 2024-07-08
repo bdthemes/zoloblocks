ac1af51df2c855d0eb2b80f12563201a4fde6df0
HelloTalib <abutalib4599@gmail.com>
2024-07-08 15:31:08 +0600

    text path and icon block added to dashboard

--- includes/Blocks/Blocks.php
+++ includes/Blocks/Blocks.php
@@ -216,7 +216,7 @@ return apply_filters('zolo_blocks_list_data', [
         'frontend-styles'  => ['zolo-block-common-style'],
         'frontend-scripts' => false
     ],
-      'progress-pie' => [
+    'progress-pie' => [
         'name'             => 'progress-pie',
         'metadata'         => trailingslashit(ZOLO_DIR_PATH) . 'blocks/progress-pie/block.json',
         'frontend-styles'  => ['zolo-block-common-style'],
@@ -283,13 +283,13 @@ return apply_filters('zolo_blocks_list_data', [
         'frontend-styles'  => ['zolo-block-common-style'],
         'frontend-scripts' => false
     ],
-    'textarea' => [
-        'name'             => 'textarea',
-        'metadata'         => trailingslashit(ZOLO_DIR_PATH) . 'blocks/textarea/block.json',
+    'icon' => [
+        'name'             => 'icon',
+        'metadata'         => trailingslashit(ZOLO_DIR_PATH) . 'blocks/icon/block.json',
         'frontend-styles'  => ['zolo-block-common-style'],
         'frontend-scripts' => false
     ],
-    'textpath' => [
+    'text-path' => [
         'name'             => 'text-path',
         'metadata'         => trailingslashit(ZOLO_DIR_PATH) . 'blocks/text-path/block.json',
         'frontend-styles'  => ['zolo-block-common-style'],
