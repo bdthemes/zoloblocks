fb04590346f12847c1781979ade3b0ab0ab60593
bdtu027 <bdtu027@bdtu027s-Mini.localdomain>
2024-07-10 15:31:15 +0600

    panel setting add and preview image   done

--- blocks/text-path/src/constants/index.js
+++ blocks/text-path/src/constants/index.js
@@ -13,3 +13,21 @@ export const TEXTPATH_SIZE = 'textPathSize';
 export const TEXT_PATH_STROKE = 'textPathStroke';
 export const TEXT_WORD_SPACING = 'textWordSpacing';
 export const PATH_TEXT_SPACING = 'textPathSpacing';
+
+export const PATH_OPTION = [
+    {
+        label: __('wave', 'zoloblocks'),
+        value: 'wave',
+    },
+    {
+        label: __('Arc', 'zoloblocks'),
+        value: 'arc',
+    },
+    {
+        label: __('Circle', 'zoloblocks'),
+        value: 'circle',
+    },
+    { label: __('Line', 'zoloblocks'), value: 'line' },
+    { label: __('Oval', 'zoloblocks'), value: 'oval' },
+    { label: __('Spiral', 'zoloblocks'), value: 'spiral' },
+];
--- blocks/text-path/src/frontend.js
+++ blocks/text-path/src/frontend.js
@@ -1,4 +1,4 @@
-import { render } from '@wordpress/element';
+import { render, createRoot } from '@wordpress/element';
 import SvgComponent from './svg';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -6,11 +6,15 @@ document.addEventListener('DOMContentLoaded', () => {
     if (textPath.length < 0) {
         return;
     }
-    const SvgItem = ({ textpathContent, textPathType, pathlink, uniqueId, textpathLength }) => {
+    const SvgItem = ({ textpathContent, textPathType, pathlink, uniqueId, textpathLength, textPathSpoint }) => {
         return (
             <SvgComponent uniqueId={uniqueId} pathType={textPathType}>
                 <text>
-                    <textPath href={`#MyPath-${uniqueId}`} textLength={textpathLength}>
+                    <textPath
+                        href={`#MyPath-${uniqueId}`}
+                        textLength={textpathLength ? textpathLength : 0}
+                        startOffset={textPathSpoint ? 100 - textPathSpoint + '%' : 0 + '%'}
+                    >
                         <a
                             className="zolo-textpath"
                             href={pathlink && pathlink.url}
@@ -30,16 +34,18 @@ document.addEventListener('DOMContentLoaded', () => {
         const textPathType = item.dataset.textpathtype;
         const pathlink = JSON.parse(item.dataset.pathlink);
         const textpathLength = item.dataset.textpathlength;
+        const textPathSpoint = item.dataset.textstartoffset;
         const uniqueId = item.dataset.uniqueid;
-        render(
+        let root = createRoot(item);
+        root.render(
             <SvgItem
                 textpathContent={textpathContent}
                 textPathType={textPathType}
                 pathlink={pathlink}
                 uniqueid={uniqueId}
                 textpathLength={textpathLength}
-            />,
-            item
+                textPathSpoint={textPathSpoint}
+            />
         );
     });
 });
