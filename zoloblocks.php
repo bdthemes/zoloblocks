d9708e8b5e4ed5eea64ec92d5451c3d53237bf61
ABU TALIB <abutalib4590@gmail.com>
2024-07-29 16:51:06 +0600

    fix issues & update changelog

--- zoloblocks.php
+++ zoloblocks.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: ZoloBlocks
  * Plugin URI: https://bdthemes.com/
- * Version: 1.2.1
+ * Version: 1.2.2-rc1
  * Author: BdThemes
  * Author URI: https://bdthemes.com/
  * Text Domain: zoloblocks
@@ -66,7 +66,7 @@ final class ZoloBlocks {
         define('ZOLO_FILE', __FILE__);
         define('ZOLO_NAMESPACE', 'zoloblocks');
         define('ZOLO_SLUG', 'zoloblocks');
-        define('ZOLO_VERSION', '1.2.1');
+        define('ZOLO_VERSION', '1.2.2-rc1');
         define('ZOLO_DIR_PATH', plugin_dir_path(__FILE__));
         define('ZOLO_ADMIN_URL', plugin_dir_url(__FILE__));
         define('ZOLO_WP_VERSION', (float) get_bloginfo('version'));
@@ -91,19 +91,19 @@ ZoloBlocks::get_instance();
  * SDK Integration
  */
 
- if ( ! function_exists( 'dci_plugin_zoloblocks' ) ) {
+if (!function_exists('dci_plugin_zoloblocks')) {
     function dci_plugin_zoloblocks() {
 
         // Include DCI SDK.
-        require_once dirname( __FILE__ ) . '/dci/start.php';
-        wp_enqueue_style( 'dci-sdk-zolo', plugins_url( 'dci/assets/css/dci.css', __FILE__ ), array(), '1.3.0', 'all' );
+        require_once dirname(__FILE__) . '/dci/start.php';
+        wp_enqueue_style('dci-sdk-zolo', plugins_url('dci/assets/css/dci.css', __FILE__), array(), '1.3.0', 'all');
 
-        dci_dynamic_init( array(
+        dci_dynamic_init(array(
             'sdk_version'         => '1.2.0',
             'product_id'          => 5,
             'plugin_name'         => 'ZoloBlocks', // make simple, must not empty
             'plugin_title'        => 'Love using ZoloBlocks? Congrats 🎉  ( Never miss an Important Update )',
-            'plugin_icon'         => plugins_url( '/', __FILE__ ) . 'assets/images/zb-brand.svg',
+            'plugin_icon'         => plugins_url('/', __FILE__) . 'assets/images/zb-brand.svg',
             'api_endpoint'        => 'https://analytics.bdthemes.com/wp-json/dci/v1/data-insights',
             'slug'                => 'zoloblocks',
             'menu'                => array(
@@ -114,8 +114,7 @@ ZoloBlocks::get_instance();
             'popup_notice'        => true,
             'deactivate_feedback' => true,
             'plugin_msg'          => '<p>Be Top-contributor by sharing non-sensitive plugin data and create an impact to the global WordPress community today! You can receive valuable emails periodically.',
-        ) );
-
+        ));
     }
-    add_action( 'admin_init', 'dci_plugin_zoloblocks' );
+    add_action('admin_init', 'dci_plugin_zoloblocks');
 }
