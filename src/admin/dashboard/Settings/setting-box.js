b2d80ce789111f1713583fd7528abc163911b259
Asikur Rahman <asikur@bdthemes.com>
2024-09-12 16:01:05 +0600

    review grid cloumn issue fixed and dashboard text translate

--- src/admin/dashboard/Settings/setting-box.js
+++ src/admin/dashboard/Settings/setting-box.js
@@ -4,10 +4,9 @@ const SettingBox = ({ title = 'Option title', description = '', children, releas
             <div className="zolo-settins-content">
                 {title && <h2 className="zolo-settings-title">{title}</h2>}
                 {description && <p className="zolo-settings-text">{description}</p>}
-                
             </div>
             {children}
-            {!released && <span className="zolo-badge-upcoming">Coming Soon</span>}
+            {!released && <span className="zolo-badge-upcoming">{__('Coming Soon', 'zoloblocks')}</span>}
         </div>
     );
 };
