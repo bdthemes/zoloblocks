06d7ec219506e40fc8f993afdfb90e630333f1b8
ABU TALIB <abutalib4590@gmail.com>
2024-09-29 10:48:51 +0600

    fix per page issue in maintenace mode

--- src/admin/dashboard/Settings/index.js
+++ src/admin/dashboard/Settings/index.js
@@ -54,7 +54,7 @@ const Settings = () => {
     }, [fetchSettings]);
 
     useEffect(() => {
-        fetchTemplates({ path: 'wp/v2/pages' });
+        fetchTemplates({ path: 'wp/v2/posts/?per_page=-1' });
     }, []);
 
     const updateSettings = useCallback(async (data) => {
