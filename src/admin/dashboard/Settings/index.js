28cfdaa553539bd4f16bc32e7442d22b570778fb
ABU TALIB <abutalib4590@gmail.com>
2024-09-29 10:52:16 +0600

    fix issues

--- src/admin/dashboard/Settings/index.js
+++ src/admin/dashboard/Settings/index.js
@@ -54,7 +54,7 @@ const Settings = () => {
     }, [fetchSettings]);
 
     useEffect(() => {
-        fetchTemplates({ path: 'wp/v2/posts/?per_page=-1' });
+        fetchTemplates({ path: 'wp/v2/pages/?per_page=-1' });
     }, []);
 
     const updateSettings = useCallback(async (data) => {
