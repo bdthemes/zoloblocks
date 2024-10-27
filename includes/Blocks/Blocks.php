99d378fde77adf41943d20315fbcedf483b39f1a
ABU TALIB <abutalib4590@gmail.com>
2024-10-27 10:11:45 +0600

    spline added to dashboard

--- includes/Blocks/Blocks.php
+++ includes/Blocks/Blocks.php
@@ -487,6 +487,14 @@ return apply_filters('zolo_blocks_list_data', [
         'status'     => true,
         'is_child'   => true,
     ],
+    'spline-viewer' => [
+        'name'       => 'spline-viewer',
+        'title'      => __('Spline Viewer', 'zoloblocks'),
+        'categories' => ['others'],
+        'status'     => true,
+        'demo'       => 'https://demo.zoloblocks.com/spline-viewer',
+        'video'      => 'https://youtu.be/8kBMIL-4PZI?list=PLP0S85GEw7DPpFyon1kxBZ8H1Ei7GK1yX',
+    ],
     'post-comments'      => [
         'name'       => 'post-comments',
         'class'      => '\Zolo\Blocks\PostComments',
