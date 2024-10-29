ef6cb5faf28e37f0d1c75d6977feab5beecc3554
Zakaria Binsaifullah <zbinsaifullah@gmail.com>
2024-10-29 10:41:56 +0600

    page templates developing going on

--- src/blocks/container/edit.js
+++ src/blocks/container/edit.js
@@ -1,17 +1,16 @@
 /**
  * WordPress dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { useEffect } from '@wordpress/element';
 import { useSelect } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
 
 /**
  * Internal depencencies
  */
 import Inspector from './inspector';
 
-import { VariationPicker } from './variations';
 import RenderView from './render-view';
+import { VariationPicker } from './variations';
 
 // import style
 import Style from './style';
