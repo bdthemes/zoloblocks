97dae56ff128822e8a74b9fc84740d8561004676
Asikur Rahman <asikur@bdthemes.com>
2024-10-29 15:13:27 +0600

    post features image blocks checking and fixing

--- src/blocks/post-featured-image/inspector.js
+++ src/blocks/post-featured-image/inspector.js
@@ -1,180 +1,189 @@
-import {__} from '@wordpress/i18n';
-import {InspectorControls} from '@wordpress/block-editor';
-import {ToggleControl, CardDivider, SelectControl, TextControl} from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { InspectorControls } from '@wordpress/block-editor';
+import { ToggleControl, CardDivider, SelectControl, TextControl } from '@wordpress/components';
 import objAttributes from './attributes';
-import {applyFilters} from "@wordpress/hooks";
+import { applyFilters } from '@wordpress/hooks';
 
 import {
-  //thumbnail
-  THUMBNAIL_ALIGN,
-  THUMBNAIL_WIDTH,
-  THUMBNAIL_HEIGHT,
-  THUMBNAIL_BORDER,
-  THUMBNAIL_BRADIUS,
-  THUMBNAIL_BOX_SHADOW,
-  THUMBNAIL_HOVER_SHADOW
+    //thumbnail
+    THUMBNAIL_ALIGN,
+    THUMBNAIL_WIDTH,
+    THUMBNAIL_HEIGHT,
+    THUMBNAIL_BORDER,
+    THUMBNAIL_BRADIUS,
+    THUMBNAIL_BOX_SHADOW,
+    THUMBNAIL_HOVER_SHADOW,
 } from './constants';
 
-import {FLEX_HORIZONTAL_OPTIONS, THUMBNAIL_SIZE} from "@/global/constants";
+import { DEFAULT_ALIGNS, THUMBNAIL_SIZE } from '@/global/constants';
 
 const {
-  ResDimensionsControl,
-  BorderControl,
-  BoxShadowControl,
-  HeaderTabs,
-  AdvancedOptions,
-  ZoloPanelBody,
-  TabPanelControl,
-  ResRangeControl,
-  ResAlignmentControl
+    ResDimensionsControl,
+    BorderControl,
+    BoxShadowControl,
+    ColorControl,
+    HeaderTabs,
+    AdvancedOptions,
+    ZoloPanelBody,
+    TabPanelControl,
+    ResRangeControl,
+    ResAlignmentControl,
 } = window.zoloModule;
 export default function Inspector(props) {
-  const {attributes, setAttributes, block} = props;
-  const {
-    resMode,
-    isLink,
-    linkTarget,
-    linkRel,
-    useFirstImageFromPost,
-    //thumbnail
-    thumbnailSize,
-  } = attributes;
-  const requiredProps = {
-    resMode,
-    setAttributes,
-    attributes,
-    objAttributes,
-  };
+    const { attributes, setAttributes, block } = props;
+    const {
+        resMode,
+        isLink,
+        linkTarget,
+        linkRel,
+        useFirstImageFromPost,
+        //thumbnail
+        thumbnailSize,
+        borderHoverColor,
+    } = attributes;
+    const requiredProps = {
+        resMode,
+        setAttributes,
+        attributes,
+        objAttributes,
+    };
 
-  // css filter
-  const cssFilters = applyFilters('zolo.extensions.controls.cssFilters', [], block, props);
-  const cssFiltersHover = applyFilters('zolo.extensions.controls.cssFiltersHover', [], block, props);
+    // css filter
+    const cssFilters = applyFilters('zolo.extensions.controls.cssFilters', [], block, props);
+    const cssFiltersHover = applyFilters('zolo.extensions.controls.cssFiltersHover', [], block, props);
 
-  return (
-    <InspectorControls key="controls">
-      <HeaderTabs
-        block="zolo/post-navigation"
-        attributes={attributes}
-        setAttributes={setAttributes}
-        generalTab={
-          <>
-            <ZoloPanelBody title={__('General', 'zoloblocks')} panelProps={props} firstOpen={true}>
-              <div className="zolo-custom-heading" style={{border: 0, paddingTop: 0}}>
-                {__('Show/hide elements', 'zoloblocks')}
-              </div>
-              <ToggleControl
-                label={__('Link to Post', 'zoloblocks')}
-                checked={isLink}
-                onChange={(isLink) => setAttributes({isLink})}
-              />
-              {isLink && (
-                <>
-                  <ToggleControl
-                    label={__('Open in new tab', 'zoloblocks')}
-                    onChange={(value) => setAttributes({linkTarget: value ? '_blank' : '_self'})}
-                    checked={linkTarget === '_blank'}
-                  />
-                  <TextControl
-                    label={__('Link rel', 'zoloblocks')}
-                    value={linkRel}
-                    onChange={(linkRel) => setAttributes({linkRel})}
-                  />
-                </>
-              )}
-              <CardDivider/>
-              <ToggleControl
-                label={__('Image From Post Content', 'zoloblocks')}
-                checked={useFirstImageFromPost}
-                onChange={(useFirstImageFromPost) => setAttributes({useFirstImageFromPost})}
-                help={__("Enable 'Use first image from post content' if no featured image is set.", 'zoloblocks')}
-              />
-              <CardDivider/>
-              <SelectControl
-                label={__('Resolution', 'zoloblocks')}
-                value={thumbnailSize}
-                options={THUMBNAIL_SIZE}
-                onChange={(thumbnailSize) => setAttributes({thumbnailSize})}
-              />
-              <ResAlignmentControl
-                label={__('Alignment', 'zoloblocks')}
-                controlName={THUMBNAIL_ALIGN}
-                requiredProps={requiredProps}
-                alignOptions={FLEX_HORIZONTAL_OPTIONS}
-              />
-            </ZoloPanelBody>
-          </>
-        }
-        styleTab={
-          <>
-            <ZoloPanelBody title={__('Image', 'zoloblocks')} firstOpen={true} stylePanel={true} panelProps={props}>
-
-              <TabPanelControl
-                normalComponents={
-                  <>
-                    <ResRangeControl
-                      label={__('Width', 'zoloblocks')}
-                      controlName={THUMBNAIL_WIDTH}
-                      requiredProps={requiredProps}
-                      min={0}
-                      max={2000}
-                      step={1}
-                    />
-                    <ResRangeControl
-                      label={__('Height', 'zoloblocks')}
-                      controlName={THUMBNAIL_HEIGHT}
-                      requiredProps={requiredProps}
-                      min={0}
-                      max={1000}
-                      step={1}
-                    />
-                    <CardDivider/>
-                    <BorderControl
-                      label={__('Border', 'zoloblocks')}
-                      controlName={THUMBNAIL_BORDER}
-                      requiredProps={requiredProps}
-                    />
-                    <ResDimensionsControl
-                      label={__('Border Radius', 'zoloblocks')}
-                      controlName={THUMBNAIL_BRADIUS}
-                      requiredProps={requiredProps}
-                      forBorderRadius={true}
-                    />
-                    <BoxShadowControl
-                      controlName={THUMBNAIL_BOX_SHADOW}
-                      requiredProps={requiredProps}
-                      enableTransition={false}
-                    />
-                    <CardDivider/>
-                    {cssFilters && cssFilters.length > 0 && cssFilters}
-                  </>
+    return (
+        <InspectorControls key="controls">
+            <HeaderTabs
+                block="zolo/post-navigation"
+                attributes={attributes}
+                setAttributes={setAttributes}
+                generalTab={
+                    <>
+                        <ZoloPanelBody title={__('General', 'zoloblocks')} panelProps={props} firstOpen={true}>
+                            <div className="zolo-custom-heading" style={{ border: 0, paddingTop: 0 }}>
+                                {__('Show/hide elements', 'zoloblocks')}
+                            </div>
+                            <ToggleControl
+                                label={__('Link to Post', 'zoloblocks')}
+                                checked={isLink}
+                                onChange={(isLink) => setAttributes({ isLink })}
+                            />
+                            {isLink && (
+                                <>
+                                    <ToggleControl
+                                        label={__('Open in new tab', 'zoloblocks')}
+                                        onChange={(value) => setAttributes({ linkTarget: value ? '_blank' : '_self' })}
+                                        checked={linkTarget === '_blank'}
+                                    />
+                                    <TextControl
+                                        label={__('Link rel', 'zoloblocks')}
+                                        value={linkRel}
+                                        onChange={(linkRel) => setAttributes({ linkRel })}
+                                    />
+                                </>
+                            )}
+                            <CardDivider />
+                            <ToggleControl
+                                label={__('Image From Post Content', 'zoloblocks')}
+                                checked={useFirstImageFromPost}
+                                onChange={(useFirstImageFromPost) => setAttributes({ useFirstImageFromPost })}
+                                help={__("Enable 'Use first image from post content' if no featured image is set.", 'zoloblocks')}
+                            />
+                            <CardDivider />
+                            <SelectControl
+                                label={__('Resolution', 'zoloblocks')}
+                                value={thumbnailSize}
+                                options={THUMBNAIL_SIZE}
+                                onChange={(thumbnailSize) => setAttributes({ thumbnailSize })}
+                            />
+                            <ResAlignmentControl
+                                label={__('Alignment', 'zoloblocks')}
+                                controlName={THUMBNAIL_ALIGN}
+                                requiredProps={requiredProps}
+                                alignOptions={DEFAULT_ALIGNS}
+                            />
+                        </ZoloPanelBody>
+                    </>
                 }
-                hoverComponents={
-                  <>
-                    <BoxShadowControl
-                      controlName={THUMBNAIL_HOVER_SHADOW}
-                      requiredProps={requiredProps}
-                      enableTransition={false}
-                    />
-                    <CardDivider/>
-                    {cssFiltersHover && cssFiltersHover.length > 0 && cssFiltersHover}
-                  </>
+                styleTab={
+                    <>
+                        <ZoloPanelBody title={__('Image', 'zoloblocks')} firstOpen={true} stylePanel={true} panelProps={props}>
+                            <TabPanelControl
+                                normalComponents={
+                                    <>
+                                        <ResRangeControl
+                                            label={__('Width', 'zoloblocks')}
+                                            controlName={THUMBNAIL_WIDTH}
+                                            requiredProps={requiredProps}
+                                            min={0}
+                                            max={2000}
+                                            step={1}
+                                        />
+                                        <ResRangeControl
+                                            label={__('Height', 'zoloblocks')}
+                                            controlName={THUMBNAIL_HEIGHT}
+                                            requiredProps={requiredProps}
+                                            min={0}
+                                            max={1000}
+                                            step={1}
+                                        />
+                                        <CardDivider />
+                                        <BorderControl
+                                            label={__('Border', 'zoloblocks')}
+                                            controlName={THUMBNAIL_BORDER}
+                                            requiredProps={requiredProps}
+                                        />
+                                        <ResDimensionsControl
+                                            label={__('Border Radius', 'zoloblocks')}
+                                            controlName={THUMBNAIL_BRADIUS}
+                                            requiredProps={requiredProps}
+                                            forBorderRadius={true}
+                                        />
+                                        <BoxShadowControl
+                                            controlName={THUMBNAIL_BOX_SHADOW}
+                                            requiredProps={requiredProps}
+                                            enableTransition={false}
+                                        />
+                                        <CardDivider />
+                                        {cssFilters && cssFilters.length > 0 && cssFilters}
+                                    </>
+                                }
+                                hoverComponents={
+                                    <>
+                                        <ColorControl
+                                            label={__('Border Color', 'zoloblocks')}
+                                            color={borderHoverColor}
+                                            onChange={(value) =>
+                                                setAttributes({
+                                                    borderHoverColor: value,
+                                                })
+                                            }
+                                        />
+                                        <BoxShadowControl
+                                            controlName={THUMBNAIL_HOVER_SHADOW}
+                                            requiredProps={requiredProps}
+                                            enableTransition={false}
+                                        />
+                                        <CardDivider />
+                                        {cssFiltersHover && cssFiltersHover.length > 0 && cssFiltersHover}
+                                    </>
+                                }
+                            />
+                        </ZoloPanelBody>
+                    </>
                 }
-              />
-
-            </ZoloPanelBody>
-          </>
-        }
-        advancedTab={
-          <>
-            <AdvancedOptions
-              attributes={attributes}
-              setAttributes={setAttributes}
-              requiredProps={requiredProps}
-              block="zolo/post-featured-image"
+                advancedTab={
+                    <>
+                        <AdvancedOptions
+                            attributes={attributes}
+                            setAttributes={setAttributes}
+                            requiredProps={requiredProps}
+                            block="zolo/post-featured-image"
+                        />
+                    </>
+                }
             />
-          </>
-        }
-      />
-    </InspectorControls>
-  );
+        </InspectorControls>
+    );
 }
--- src/blocks/post-featured-image/styles.js
+++ src/blocks/post-featured-image/styles.js
@@ -1,116 +1,111 @@
-import {applyFilters} from '@wordpress/hooks';
+import { applyFilters } from '@wordpress/hooks';
 
 import {
-  //thumbnail
-  THUMBNAIL_ALIGN,
-  THUMBNAIL_WIDTH,
-  THUMBNAIL_HEIGHT,
-  THUMBNAIL_BORDER,
-  THUMBNAIL_BRADIUS,
-  THUMBNAIL_BOX_SHADOW,
-  THUMBNAIL_HOVER_SHADOW
+    //thumbnail
+    THUMBNAIL_ALIGN,
+    THUMBNAIL_WIDTH,
+    THUMBNAIL_HEIGHT,
+    THUMBNAIL_BORDER,
+    THUMBNAIL_BRADIUS,
+    THUMBNAIL_BOX_SHADOW,
+    THUMBNAIL_HOVER_SHADOW,
 } from './constants';
 
 const {
-  generateResAlignmentStyle,
-  generateResRangeStyle,
-  generateDimensionStyle,
-  generateBorderStyle,
-  generateBoxShadowStyles,
-  GlobalStyleHanlder
+    generateResAlignmentStyle,
+    generateResRangeStyle,
+    generateDimensionStyle,
+    generateBorderStyle,
+    generateBoxShadowStyles,
+    GlobalStyleHanlder,
 } = window.zoloModule;
 
-function Style({props}) {
-  const {attributes, setAttributes} = props;
-  const {
-    uniqueId,
-  } = attributes;
+function Style({ props }) {
+    const { attributes, setAttributes } = props;
+    const { uniqueId, borderHoverColor } = attributes;
 
-  //thumbnail
-  const {
-    desktopAlignStyle: thumbAlignDesk,
-    tabAlignStyle: thumbAlignTab,
-    mobAlignStyle: thumbAlignMob,
-  } = generateResAlignmentStyle({
-    controlName: THUMBNAIL_ALIGN,
-    property: 'text-align',
-    attributes,
-  });
-  const {
-    desktopRangeStyle: thumbWidthDesk,
-    tabRangeStyle: thumbWidthTab,
-    mobRangeStyle: thumbWidthMob,
-  } = generateResRangeStyle({
-    controlName: THUMBNAIL_WIDTH,
-    property: 'width',
-    attributes,
-  });
-  const {
-    desktopRangeStyle: thumbHeightDesk,
-    tabRangeStyle: thumbHeightTab,
-    mobRangeStyle: thumbHeightMob,
-  } = generateResRangeStyle({
-    controlName: THUMBNAIL_HEIGHT,
-    property: 'height',
-    attributes,
-  });
-  const {
-    desktopBorderStyle: thumbBorderDesk,
-    tabBorderStyle: thumbBorderTab,
-    mobBorderStyle: thumbBorderMob,
-  } = generateBorderStyle({
-    controlName: THUMBNAIL_BORDER,
-    attributes,
-  });
-  const {
-    dimensionStylesDesktop: thumbBRadiusDesk,
-    dimensionStylesTab: thumbBRadiusTab,
-    dimensionStylesMobile: thumbBRadiusMob,
-  } = generateDimensionStyle({
-    controlName: THUMBNAIL_BRADIUS,
-    styleFor: 'border-radius',
-    attributes,
-  });
-  const {boxShadowStyle: thumbBoxShadow} = generateBoxShadowStyles({
-    attributes,
-    controlName: THUMBNAIL_BOX_SHADOW,
-  });
-  const {boxShadowStyle: thumbHoverBoxShadow} = generateBoxShadowStyles({
-    attributes,
-    controlName: THUMBNAIL_HOVER_SHADOW,
-  });
-  const {
-    active = false,
-    blur = 0,
-    brightness = 100,
-    contrast = 100,
-    saturate = 100,
-    hueRotate = 0
-  } = attributes?.cssFilters || {};
-  const {
-    active: activeHover = false,
-    blur: blurHover = 0,
-    brightness: brightnessHover = 100,
-    contrast: contrastHover = 100,
-    saturate: saturateHover = 100,
-    hueRotate: hueRotateHover = 0,
-  } = attributes?.cssFiltersHover || {};
+    //thumbnail
+    const {
+        desktopAlignStyle: thumbAlignDesk,
+        tabAlignStyle: thumbAlignTab,
+        mobAlignStyle: thumbAlignMob,
+    } = generateResAlignmentStyle({
+        controlName: THUMBNAIL_ALIGN,
+        property: 'text-align',
+        attributes,
+    });
+    const {
+        desktopRangeStyle: thumbWidthDesk,
+        tabRangeStyle: thumbWidthTab,
+        mobRangeStyle: thumbWidthMob,
+    } = generateResRangeStyle({
+        controlName: THUMBNAIL_WIDTH,
+        property: 'width',
+        attributes,
+    });
+    const {
+        desktopRangeStyle: thumbHeightDesk,
+        tabRangeStyle: thumbHeightTab,
+        mobRangeStyle: thumbHeightMob,
+    } = generateResRangeStyle({
+        controlName: THUMBNAIL_HEIGHT,
+        property: 'height',
+        attributes,
+    });
+    const {
+        desktopBorderStyle: thumbBorderDesk,
+        tabBorderStyle: thumbBorderTab,
+        mobBorderStyle: thumbBorderMob,
+    } = generateBorderStyle({
+        controlName: THUMBNAIL_BORDER,
+        attributes,
+    });
+    const {
+        dimensionStylesDesktop: thumbBRadiusDesk,
+        dimensionStylesTab: thumbBRadiusTab,
+        dimensionStylesMobile: thumbBRadiusMob,
+    } = generateDimensionStyle({
+        controlName: THUMBNAIL_BRADIUS,
+        styleFor: 'border-radius',
+        attributes,
+    });
+    const { boxShadowStyle: thumbBoxShadow } = generateBoxShadowStyles({
+        attributes,
+        controlName: THUMBNAIL_BOX_SHADOW,
+    });
+    const { boxShadowStyle: thumbHoverBoxShadow } = generateBoxShadowStyles({
+        attributes,
+        controlName: THUMBNAIL_HOVER_SHADOW,
+    });
+    const { active = false, blur = 0, brightness = 100, contrast = 100, saturate = 100, hueRotate = 0 } = attributes?.cssFilters || {};
+    const {
+        active: activeHover = false,
+        blur: blurHover = 0,
+        brightness: brightnessHover = 100,
+        contrast: contrastHover = 100,
+        saturate: saturateHover = 100,
+        hueRotate: hueRotateHover = 0,
+    } = attributes?.cssFiltersHover || {};
 
-
-  const desktopAllStyle = `
+    const desktopAllStyle = `
     .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image{
       ${thumbAlignDesk}
+    }
+    .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image img{
       ${thumbWidthDesk}
       ${thumbHeightDesk}
       ${thumbBorderDesk}
       ${thumbBRadiusDesk}
       ${thumbBoxShadow}
     }
-     .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image:hover{
+     .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image:hover img{
       ${thumbHoverBoxShadow}
+      ${borderHoverColor ? `border-color: ${borderHoverColor};` : ''}
      }
 
-     ${active ? `
+     ${
+         active
+             ? `
         .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image img {
             filter:
                 blur(${blur}px)
@@ -119,10 +114,14 @@ function Style({props}) {
                 saturate(${saturate}%)
                 hue-rotate(${hueRotate}deg)
         }
-   ` : ''}
+   `
+             : ''
+     }
 
-      ${activeHover ? `
-          .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image img:hover {
+      ${
+          activeHover
+              ? `
+          .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image:hover img {
               filter:
                   blur(${blurHover}px)
                   brightness(${brightnessHover}%)
@@ -130,20 +129,27 @@ function Style({props}) {
                   saturate(${saturateHover}%)
                   hue-rotate(${hueRotateHover}deg)
           }
-     ` : ''}
+     `
+              : ''
+      }
   `;
 
-  const tabletAllStyle = `
+    const tabletAllStyle = `
     .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image{
       ${thumbAlignTab}
+    }
+    .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image img{
       ${thumbWidthTab}
       ${thumbHeightTab}
       ${thumbBorderTab}
       ${thumbBRadiusTab}
     }
   `;
-  const mobileAllStyle = `
+    const mobileAllStyle = `
     .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image{
+      ${thumbAlignMob}
+    }
+    .${uniqueId}.zolo-block.wp-block-zolo-post-featured-image img{
       ${thumbAlignMob}
       ${thumbWidthMob}
       ${thumbHeightMob}
@@ -151,17 +157,17 @@ function Style({props}) {
       ${thumbBRadiusMob}
     }
   `;
-  return (
-    <>
-      <GlobalStyleHanlder
-        attributes={attributes}
-        setAttributes={setAttributes}
-        desktopAllStyle={applyFilters('zolo.postFeaturedImage.desktopAllStyle', desktopAllStyle, props)}
-        tabAllStyle={applyFilters('zolo.postFeaturedImage.tabletAllStyle', tabletAllStyle, props)}
-        mobileAllStyle={applyFilters('zolo.postFeaturedImage.mobileAllStyle', mobileAllStyle, props)}
-      />
-    </>
-  );
+    return (
+        <>
+            <GlobalStyleHanlder
+                attributes={attributes}
+                setAttributes={setAttributes}
+                desktopAllStyle={applyFilters('zolo.postFeaturedImage.desktopAllStyle', desktopAllStyle, props)}
+                tabAllStyle={applyFilters('zolo.postFeaturedImage.tabletAllStyle', tabletAllStyle, props)}
+                mobileAllStyle={applyFilters('zolo.postFeaturedImage.mobileAllStyle', mobileAllStyle, props)}
+            />
+        </>
+    );
 }
 
 export default Style;
