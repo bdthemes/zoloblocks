a69e1339f4cfc49c70a490c13dbe582e9686dc6e
shamim496 <mdshamimhasan496@gmail.com>
2024-11-13 11:41:37 +0600

    advanced paragraph control add

--- src/blocks/advanced-paragraph/save.js
+++ src/blocks/advanced-paragraph/save.js
@@ -5,28 +5,21 @@ const { classArrayToStr, DisplayZoloIcon } = window.zoloModule;
 
 const Save = (props) => {
     const { attributes } = props;
-    const { uniqueId, parentClasses } = attributes;
+    const { uniqueId, parentClasses, content } = attributes;
 
     const blockProps = useBlockProps.save({
         className: classnames(uniqueId, classArrayToStr(parentClasses)),
     });
 
     return (
-        // <div
-        //     {...blockProps}
-        //     {...(zoloId && {
-        //         id: zoloId,
-        //     })}
-        // >
         <RichText.Content
             {...blockProps}
             tagName="div"
-            value={props.attributes.content}
+            value={content}
             formatingcontrols={['bold', 'italic']}
             placeholder="Enter text..."
             multiline="p"
         />
-        // </div>
     );
 };
 
