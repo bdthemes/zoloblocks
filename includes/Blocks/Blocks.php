5ed769f4dea734b6317fb3220862fe3387d70008
ABU TALIB <abutalib4590@gmail.com>
2024-11-25 10:04:35 +0600

    single page block merging issue fixed

--- includes/Blocks/Blocks.php
+++ includes/Blocks/Blocks.php
@@ -670,7 +670,7 @@ return apply_filters('zolo_blocks_list_data', [
         'status'     => true,
         'demo'       => 'https://demo.zoloblocks.com/post-content',
         'video'      => 'https://youtu.be/8kBMIL-4PZI?list=PLP0S85GEw7DPpFyon1kxBZ8H1Ei7GK1yX',
-    ]
+    ],
     'advanced-paragraph' => [
         'name' => 'advanced-paragraph',
         'title' => __('Advanced Paragraph', 'zoloblocks-pro'),
