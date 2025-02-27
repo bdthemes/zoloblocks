1e9021a43f889c297ce49c0879628fdd53456f33
Mijanur Rahman <mahmudmizan717@gmail.com>
2025-02-27 20:39:37 +0600

    improve single blocks

--- src/blocks/post-comments-form/index.js
+++ src/blocks/post-comments-form/index.js
@@ -1,36 +1,18 @@
+import { registerBlockType } from '@wordpress/blocks';
 import metadata from './block.json';
 import attributes from './attributes';
 import Edit from './edit';
 import './style.scss';
 
 const {BlockIcons} = window.zoloIcons;
-const {manageSingleBlock} = window.zoloModule;
 
-const blockConfig = {
-  metadata,
-  icon: BlockIcons['post-comments-form'],
+registerBlockType(metadata.name, {
+  icon: {
+      src: BlockIcons['post-comments-form'],
+  },
   attributes,
   edit: Edit,
-};
-
-// Register/unregister the block based on the URL
-manageSingleBlock(blockConfig);
-
-// Optional: Add listeners for dynamic URL updates
-window.addEventListener('popstate', () => manageSingleBlock(blockConfig));
-
-// Override history.pushState to detect programmatic URL changes
-const originalPushState = history.pushState;
-history.pushState = function (...args) {
-  originalPushState.apply(history, args);
-  manageSingleBlock(blockConfig);  // Re-check when URL is updated programmatically
-};
-
-// Override history.replaceState to detect programmatic URL changes
-const originalReplaceState = history.replaceState;
-history.replaceState = function (...args) {
-  originalReplaceState.apply(history, args);
-  manageSingleBlock(blockConfig);  // Re-check when URL is updated programmatically
-};
+  save: () => null,
+});
 
 
