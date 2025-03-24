dda5236cf9b7178a364f3ba1971c3d9a3f34d3c5
Mijanur Rahman <mahmudmizan717@gmail.com>
2025-03-24 10:09:41 +0600

    Update index.js

--- src/template-library/components/header/index.js
+++ src/template-library/components/header/index.js
@@ -72,33 +72,7 @@ const Header = ({ setIsOpen }) => {
                         }}
                     />
                 </div>
-                {/* <div className="sync-btn">
-                    <Tooltip text={__('Sync Demos', 'zoloblocks')} placement="top">
-                        <button
-                            className="sync-button"
-                            onClick={() => {
-                                syncDemos(activeTab);
-                            }}
-                        >
-                            <svg
-                                className="w-6 h-6 text-gray-800 dark:text-white"
-                                aria-hidden="true"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M17.651 7.65a7.131 7.131 0 0 0-12.68 3.15M18.001 4v4h-4m-7.652 8.35a7.13 7.13 0 0 0 12.68-3.15M6 20v-4h4"
-                                />
-                            </svg>
-                        </button>
-                    </Tooltip>
-                </div> */}
+                
                 <div className="close-btn">
                     <button onClick={() => setIsOpen(false)}>
                         <svg viewBox="0 0 24 24" width={24} height={24} color={'#000000'} fill={'none'}>
