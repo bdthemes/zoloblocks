f5c2fb8a09f3cb111c68d211300dc7d22c87521b
ABU TALIB <abutalib4590@gmail.com>
2025-05-14 14:39:48 +0600

    code updated

--- src/controls/toggle-group/index.js
+++ src/controls/toggle-group/index.js
@@ -3,17 +3,22 @@ import { ZoloToggleGroupControl, ZoloToggleGroupControlOption } from '../core-co
 const ToggleGroup = ({ label, value, onChange, options, ...props }) => {
     return (
         <div className="zolo-toggle-group-control">
-            <ZoloToggleGroupControl label={label || ''} value={value} onChange={onChange} isBlock {...props}>
-                {options &&
-                    options.map((element, index) => (
-                        <ZoloToggleGroupControlOption
-                            key={index}
-                            value={element.value}
-                            label={element?.icon ? element?.icon : element.label}
-                            aria-label={element.label}
-                            showTooltip
-                        />
-                    ))}
+            <ZoloToggleGroupControl
+                label={label || ''}
+                value={value}
+                onChange={onChange}
+                isBlock
+                {...props}
+            >
+                {options?.map((element, index) => (
+                    <ZoloToggleGroupControlOption
+                        key={element.value || index}
+                        value={element.value}
+                        label={element?.icon || element.label}
+                        aria-label={element.label}
+                        showTooltip
+                    />
+                ))}
             </ZoloToggleGroupControl>
         </div>
     );
