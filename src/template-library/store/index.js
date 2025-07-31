53d78bdfaad6806aea30cdf61a0df97f1ced207c
ABU TALIB <abutalib4590@gmail.com>
2025-07-31 12:28:50 +0600

    share template library to the blynex themes

--- src/template-library/store/index.js
+++ src/template-library/store/index.js
@@ -1,15 +1,37 @@
-import { createReduxStore, register } from '@wordpress/data';
+// import { createReduxStore, register } from '@wordpress/data';
+// import reducer from './reducer';
+// import * as selectors from './selectors';
+// import * as actions from './actions';
+// import * as resolvers from './resolvers';
+// export const STORE_NAME = 'zolo/template-library';
+
+// const store = createReduxStore(STORE_NAME, {
+//     reducer,
+//     selectors,
+//     actions,
+//     resolvers,
+// });
+
+// register(store);
+
+import { createReduxStore, register, select } from '@wordpress/data';
 import reducer from './reducer';
 import * as selectors from './selectors';
 import * as actions from './actions';
 import * as resolvers from './resolvers';
 export const STORE_NAME = 'zolo/template-library';
 
-const store = createReduxStore(STORE_NAME, {
-    reducer,
-    selectors,
-    actions,
-    resolvers,
-});
+// Check if the store is already registered before creating and registering it
+let store;
+if (!select(STORE_NAME)) {
+    store = createReduxStore(STORE_NAME, {
+        reducer,
+        selectors,
+        actions,
+        resolvers,
+    });
 
-register(store);
+    register(store);
+}
+
+export default store;
