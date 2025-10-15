ad6a8c05ca6d2b8198f055d26cc40b03ced62f52
ABU TALIB <abutalib4590@gmail.com>
2025-10-15 12:33:40 +0600

    fix title block

--- includes/Blocks/PostTitle.php
+++ includes/Blocks/PostTitle.php
@@ -30,25 +30,33 @@ class PostTitle {
 	 * @param array  $block .
 	 * @return false|string
 	 */
-	public function render( $attributes, $content, $block ) {
-		if ( ! isset( $block->context['postId'] ) ) {
+	public function render($attributes, $content, $block) {
+		if (! isset($block->context['postId'])) {
 			return '';
 		}
 
 		$title = get_the_title();
-		if ( ! $title ) {
+		if (! $title) {
 			return '';
 		}
 
-		$settings           = wp_parse_args( $attributes, $this->default_block_attributes );
-		$wrapper_class      = trim( ZoloHelpers::get_wrapper_class( $settings, '' ) . ' ' . implode( ' ', $settings['parentClasses'] ?? [] ) );
-		$wrapper_attributes = get_block_wrapper_attributes( [ 'class' => esc_attr( $wrapper_class ) ] );
-		$title              = ! empty( $settings['titleWords'] ) ? ZoloHelpers::wordcount( $title, $settings['titleWords'] ) : $title;
-		$tag_name           = ZoloHelpers::sanitize_html_tag( $settings['titleTag'] ?? 'h2' );
+		$settings           = wp_parse_args($attributes, $this->default_block_attributes);
+		$wrapper_class      = trim(ZoloHelpers::get_wrapper_class($settings, '') . ' ' . implode(' ', $settings['parentClasses'] ?? []));
+		$wrapper_attributes = get_block_wrapper_attributes(['class' => esc_attr($wrapper_class)]);
+		$title              = ! empty($settings['titleWords']) ? ZoloHelpers::wordcount($title, $settings['titleWords']) : $title;
 
-		if ( ! empty( $settings['isLink'] ) ) {
-			$rel   = ! empty( $settings['linkRel'] ) ? 'rel="' . esc_attr( $settings['linkRel'] ) . '"' : '';
-			$title = sprintf( '<a href="%1$s" target="%2$s" %3$s>%4$s</a>', esc_url( get_the_permalink( $block->context['postId'] ) ), esc_attr( $settings['linkTarget'] ), $rel, $title );
+		// Sanitize tag name
+		$tag_name     = ZoloHelpers::sanitize_html_tag($settings['titleTag'] ?? 'h2');
+
+		if (! empty($settings['isLink'])) {
+			$rel   = ! empty($settings['linkRel']) ? 'rel="' . esc_attr($settings['linkRel']) . '"' : '';
+			$title = sprintf(
+				'<a href="%1$s" target="%2$s" %3$s>%4$s</a>',
+				esc_url(get_the_permalink($block->context['postId'])),
+				esc_attr($settings['linkTarget']),
+				$rel,
+				esc_html($title)
+			);
 		}
 
 		return sprintf(
