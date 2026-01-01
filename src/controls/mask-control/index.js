01bbb83e3b48654feb3ce8e6202c235f4e854888
shamim496 <mdshamimhasan496@gmail.com>
2026-01-01 16:10:39 +0600

    mask issue fix

--- src/controls/mask-control/index.js
+++ src/controls/mask-control/index.js
@@ -3,6 +3,8 @@ import { MediaUpload } from '@wordpress/block-editor';
 
 import { ZoloSelectControl, ZoloBaseControl, ZoloButton } from '../core-controls';
 import ImageAvatar from '../image-avatar';
+import ThumbsControl from '../thumbs-control';
+import PopoverControl from '../popover-control';
 
 import { MASK_SIZES, MASK_POSITIONS, MASK_REPEATS, MASK_SHAPES } from '../../global/constants';
 
@@ -19,58 +21,32 @@ const MaskControl = ({ controlName, requiredProps }) => {
 
     return (
         <>
-            <ZoloSelectControl
+            <PopoverControl
                 label={__('Mask Shape', 'zoloblocks')}
-                value={maskImage}
-                options={MASK_SHAPES}
-                onChange={(v) =>
-                    setAttributes({
-                        [`${controlName}MaskImage`]: v,
-                    })
+                icon={
+                    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
+                        <path
+                            d="M2 19.9893V2.99264C2 2.44013 2.43656 2 2.96407 2H21.0359C21.5725 2 22 2.4495 22 2.99264V17.0863C22 17.7699 21.3543 18.2475 20.7267 18.0321C19.0896 17.4609 16.0609 16.9833 12.7958 19.315C9.31241 21.7966 5.41064 23.117 2.3638 20.7853C2.12733 20.6073 2 20.2983 2 19.9986V19.9893Z"
+                            stroke="#4D4D4D"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
                 }
-            />
-            {maskImage === 'custom' && (
-                <ZoloBaseControl label={__('Custom Mask', 'zoloblocks')} className="zolo-flex-col-control">
-                    {customMaskImage ? (
-                        <ImageAvatar
-                            imageUrl={customMaskImage && customMaskImage.url}
-                            onDeleteImage={() =>
-                                setAttributes({
-                                    [`${controlName}CustomMaskImage`]: null,
-                                })
-                            }
-                            imageId={customMaskImage && customMaskImage.id}
-                            onEditImage={(media) => {
-                                setAttributes({
-                                    [`${controlName}CustomMaskImage`]: media,
-                                });
-                            }}
-                        />
-                    ) : (
-                        <MediaUpload
-                            onSelect={(media) => {
-                                setAttributes({
-                                    [`${controlName}CustomMaskImage`]: {
-                                        id: media.id,
-                                        url: media.url,
-                                    },
-                                });
-                            }}
-                            allowedTypes={['image']}
-                            value={customMaskImage && customMaskImage.id}
-                            render={({ open }) => (
-                                <ZoloButton className="zolo-image-upload-btn" onClick={open}>
-                                    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
-                                        <path d="M11.492 10.172l-2.5 3.064-.737-.677 3.737-4.559 3.753 4.585-.753.665-2.5-3.076v7.826h-1v-7.828zm7.008 9.828h-13c-2.481 0-4.5-2.018-4.5-4.5 0-2.178 1.555-4.038 3.698-4.424l.779-.14.043-.789c.185-3.448 3.031-6.147 6.48-6.147 3.449 0 6.295 2.699 6.478 6.147l.044.789.78.14c2.142.386 3.698 2.246 3.698 4.424 0 2.482-2.019 4.5-4.5 4.5m.978-9.908c-.212-3.951-3.472-7.092-7.478-7.092s-7.267 3.141-7.479 7.092c-2.57.463-4.521 2.706-4.521 5.408 0 3.037 2.463 5.5 5.5 5.5h13c3.037 0 5.5-2.463 5.5-5.5 0-2.702-1.951-4.945-4.522-5.408" />
-                                    </svg>
-                                    {__(' Upload Mask', 'zoloblocks')}
-                                </ZoloButton>
-                            )}
-                        />
-                    )}
-                </ZoloBaseControl>
-            )}
-            {maskImage && maskImage !== undefined && (
+            >
+                <ThumbsControl
+                    value={maskImage}
+                    options={MASK_SHAPES}
+                    onChange={(selectedOption) =>
+                        setAttributes({
+                            [`${controlName}MaskImage`]: selectedOption.value,
+                            [`${controlName}CustomMaskImage`]: selectedOption.image ? { url: selectedOption.image } : null,
+                        })
+                    }
+                />
+            </PopoverControl>
+            {maskImage && maskImage !== undefined && maskImage !== '' && (
                 <>
                     <ZoloSelectControl
                         label={__('Mask Size', 'zoloblocks')}
