37e3a680679e3c631aeb408b61587ae99ff4bb59
ABU TALIB <abutalib4590@gmail.com>
2026-01-27 11:56:24 +0600

    advanced heading separator issue] fixed

--- src/blocks/advanced-heading/save.js
+++ src/blocks/advanced-heading/save.js
@@ -118,6 +118,11 @@ const Save = (props) => {
                             {showSubTitleBadgeIcon && <DisplayZoloIcon icon={subTitleBadgeIcon} />}
                         </div>
                     ))}
+                {showSeparator && separatorPosition === 'bottom' && (
+                    <div className="zolo-separator-wrapper">
+                        <div className="zolo-ah-separator"></div>
+                    </div>
+                )}
             </div>
             {renderHookAfter && renderHookAfter}
         </div>
